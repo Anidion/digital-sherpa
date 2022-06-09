8880d0737f4131d146ee518100ad092a41a9b5b9
Ben Schmidt <ben@schmidtfam.us>
2022-06-09 17:52:28 +0200

    Migrated to typescript

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
-import TopBar from "./components/TopBar.js";
+import TopBar from "./components/TopBar";
 import Main from "./pages/Main";
 
 const container = document.getElementById("root");
-const root = createRoot(container);
 
-root.render(
+ReactDOM.render(
 	<Router>
 		<TopBar />
 		<Routes>
 			<Route path="/" element={<Main />} />
 		</Routes>
 	</Router>
+
+,container
 );
